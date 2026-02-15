fbea6babf29865783d290fc0f22f4eb81dc417c7
Noam Lemel <noam123435@gmail.com>
2026-02-15 23:09:10 +0200

    drill 02 fixed

--- advance-js-arrays.js
+++ advance-js-arrays.js
@@ -14,15 +14,16 @@ console.log('myForEach: ',arrString);
 
 //second drill
 let arr2=[...arr,4,5,6,7,8,9,10];
-const mySlice=(from=0,to=arr2.length)=>{
+const mySlice=(arr,from=0,to=arr2.length)=>{
     let slice=[];
+    from=from>0?from:0;
+    to=to>arr.length?arr.length:to;
     for(let i=from;i<to;i++){
-        if(i<0||i>(arr2.length-1)) continue;
-            slice.push(arr2[i])
+            slice.push(arr[i])
     }
     return slice;
 }
-console.log(mySlice(2,6));
-console.log(mySlice(5,12));
-console.log(mySlice(-2,4));
-console.log(mySlice());
+console.log(mySlice(arr2,2,6));
+console.log(mySlice(arr2,5,12));
+console.log(mySlice(arr2,-2,4));
+console.log(mySlice(arr2));
