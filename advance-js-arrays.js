e6fb5a97609c2736ddf0301efa56862b4171546f
Noam Lemel <noam123435@gmail.com>
2026-02-15 23:01:34 +0200

    drill 02

--- advance-js-arrays.js
+++ advance-js-arrays.js
@@ -1,3 +1,4 @@
+//first drill
 const arr=[1,2,3];
 const myForEach=(arr,callback)=>{
     for(let i=0;i<arr.length;i++){
@@ -10,3 +11,18 @@ myForEach(arr,(el,i)=>{
     arrString+=`${i}:${el}  `;
 })
 console.log('myForEach: ',arrString);
+
+//second drill
+let arr2=[...arr,4,5,6,7,8,9,10];
+const mySlice=(from=0,to=arr2.length)=>{
+    let slice=[];
+    for(let i=from;i<to;i++){
+        if(i<0||i>(arr2.length-1)) continue;
+            slice.push(arr2[i])
+    }
+    return slice;
+}
+console.log(mySlice(2,6));
+console.log(mySlice(5,12));
+console.log(mySlice(-2,4));
+console.log(mySlice());
